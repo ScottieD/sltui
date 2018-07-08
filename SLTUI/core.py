61ace6bc86ad569d49ba2f18e34474e59a189071
Scott <davis.scottie@gmail.com>
2018-07-07 23:37:54 -0500

    Added Keyboard interrupt handler.

--- SLTUI/core.py
+++ SLTUI/core.py
@@ -3,19 +3,33 @@ from curses import wrapper
 import sys
 
 def main(stdscr):
+    home()
+
+
+def getconfig():
+    return
+
+
+def home():
     while True:
         # Clear screen
-        stdscr.clear()
-        stdscr.nodelay(True)
+        #stdscr.clear()
+        #stdscr.nodelay(True)
 
         #init curses object
         interface = curses.initscr()
         interface.border(0)
-        interface.addstr(5,5, "Softlayer:Control:Home")
+        interface.addstr(0, 30, "Softlayer:Setup", curses.A_TOP)
 
         #refresh screen
         interface.refresh()
-        interface.getch()
+        #interface.getch()
 
-        curses.endwin()
-wrapper(main)
+        #curses.endwin()
+
+if __name__ == "__main__":
+    try:
+        wrapper(main)
+    except KeyboardInterrupt:
+        sys.exit(1)
+sys.exit(1)
